1bb816bb8d647a6497917da7414751d3eb215584
Manuela <manuela@Ubuntu.station>
2017-03-16 18:30:09 +0100

    server and client working

--- src/clj/server.clj
+++ src/clj/server.clj
@@ -5,21 +5,36 @@
             [org.httpkit.server :as hk :refer [with-channel on-close on-receive websocket? run-server send!]]
             [org.httpkit.timer :as tim]
             [taoensso.timbre :as timbre]
+            [clojure.edn :as edn]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 
-(defonce channels (atom #{}))
+(defonce channels (atom []))
+
+
 
   (defn connect! [channel]
-   (timbre/info "channel open")
-   (swap! channels conj channel))
+   (swap! channels conj channel)
+   (println (count @channels) "channel opened")
+    ; (timbre/info  "channel open on " (str channel)  )
+    ) ;adds channel to the channels atom
 ;
+; (defn handler [request]
+;  (with-channel request channel
+;                (connect! channel)
+;               (on-close channel (fn [status] (println "channel closed: " status)))
+;                (on-receive channel(fn [data] (let [ data (edn/read-string data)
+;                                                     name (:name data)
+;                                                     message (:msg data)]
+;                                                     (println "data received: " name "and " message)) ;; echo it back
+;                           (send! channel data)))))
+
 (defn handler [request]
  (with-channel request channel
                (connect! channel)
               (on-close channel (fn [status] (println "channel closed: " status)))
-               (on-receive channel(fn [data] (println "data received: " data) ;; echo it back
-                          (send! channel data)))))
+               (on-receive channel(fn [data]  (doseq [chan @channels]
+                                                      (send! chan data))))))
 
 
 (defroutes all-routes
--- src/cljs/chat/client.cljs
+++ src/cljs/chat/client.cljs
@@ -1,24 +1,28 @@
 (ns chat.client
-  (:require [domina.core :refer [
+  (:require [domina.core :refer [append!
                                  by-id
                                  set-value!
                                  destroy!
                                  value]]
             [domina.events :refer [listen!]]
-            ))
+            [hiccups.runtime]
+            )
+            (:require-macros [hiccups.core :refer [html]]))
 
 (def ws (js/WebSocket. "ws://localhost:3000/ws"))
+
+(defn append-message [msg] (append! (by-id "history") (html [:div msg])) )
 
 ;aset => set property. (set! (.-property obj) value)
 (aset ws "onerror" (fn [](.log js/console  "error")))
 (aset ws "onopen" (fn [e] (.log js/console "websocket open!") ))
 (aset ws "onclose" (fn [e] (.log js/console "Error occurred")))
-(aset ws "onmessage" (fn [e] ((if (string? (.-data e) ) (.log js/console "Received message")))))
+(aset ws "onmessage" (fn [e]  (append-message (aget e "data"))))
 
 (defn server-message []
-  (let [name (value (by-id "name"))
-        message (value (by-id "mess"))]
-        (.send ws (str name ":" message)) ))
+(let [name (value (by-id "name"))
+      message (value (by-id "mess"))]
+      (.send ws (str message)) ))
 
 (defn ^:export init []
   (if (and js/document
